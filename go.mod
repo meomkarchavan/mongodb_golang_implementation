d2e98cd40a483410040d202103ce385fe903cd4c
meomkarchavan <meomkarchavan99@gmail.com>
2021-08-20 14:09:18 +0530

    minor fixes

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.7.4
-	github.com/go-playground/validator v9.31.0+incompatible
 	go.mongodb.org/mongo-driver v1.7.1
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
 )
--- src/controller/controller.go
+++ src/controller/controller.go
@@ -9,10 +9,22 @@ import (
 func RegisterRoutes() *gin.Engine {
 	r := gin.Default()
 
-	r.LoadHTMLGlob("D:\\GO_Workspace\\src\\mongo_go\\templates*.html")
+	r.LoadHTMLGlob("D:\\GO_Workspace\\src\\mongo_go\\templates\\*.html")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	r.GET("/add", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "add-user.html", nil)
+	})
+	r.GET("/find", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "find-user.html", nil)
+	})
+	r.GET("/delete", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "delete-user.html", nil)
+	})
+	r.GET("/update", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "update-user.html", nil)
 	})
 
 	r.Static("/public", "D:\\GO_Workspace\\src\\mongo_go\\public")
